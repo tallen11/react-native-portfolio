bba22d1631db589412e7b25a43bd5297df70d76f
Tate Allen <tatea7@yahoo.com>
2019-11-10 14:04:07 -0500

    Updates

---
+++ precache-manifest.0bb6fad0633a1132c2548c0700304acb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad621916f291182a4db7e2d238af00e2",
+    "url": "/react-native-portfolio/index.html"
+  },
+  {
+    "revision": "5107a0950783c08686ca",
+    "url": "/react-native-portfolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d09720d139a59d5e737b",
+    "url": "/react-native-portfolio/static/js/2.56459eed.chunk.js"
+  },
+  {
+    "revision": "5107a0950783c08686ca",
+    "url": "/react-native-portfolio/static/js/main.40016494.chunk.js"
+  },
+  {
+    "revision": "dbbe35995b93f3d8229b",
+    "url": "/react-native-portfolio/static/js/runtime-main.3f301a09.js"
+  },
+  {
+    "revision": "fa0ddfff09a981189830d167ff0b721e",
+    "url": "/react-native-portfolio/static/media/astronomen.fa0ddfff.webm"
+  },
+  {
+    "revision": "034cdb82c9f0a7904708eb0938875f19",
+    "url": "/react-native-portfolio/static/media/cryptodemo.034cdb82.webm"
+  }
+]);
